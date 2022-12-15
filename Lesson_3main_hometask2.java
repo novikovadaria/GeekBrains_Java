89aacd347e567209229899d48c1e34e777682a91
Daria N <107907983+novikovadaria@users.noreply.github.com>
2022-12-15 16:50:31 +0300

    Update Lesson_3main_hometask2.java

--- Lesson_3main_hometask2.java
+++ Lesson_3main_hometask2.java
@@ -14,7 +14,7 @@ public class Lesson_3main_hometask2 {
         ListTovar.add(tovar2);
         ListTovar.add(tovar3);
 
-
+        // Спрашиваем нужный сорт         
         Scanner sc = new Scanner(System.in);
         System.out.println("Введите сорт: ");
         String qual = sc.nextInt();
@@ -22,24 +22,31 @@ public class Lesson_3main_hometask2 {
         String search = qual;
         Integer totalExportVolume = 0;
         
+        // Объявляем листы         
         List<Double> ListGeneral = new ArrayList<>();
         List<Double> ListProduct = new ArrayList<>();
         
         for (int i=0; i <ListTovar.size(); i++) {
             List<Double> ListProduct = new ArrayList<>();
             if (ListTovar.get(1).getQuality().equals(search)) {
+                // Если товар подходит по сорту, добавляем инфу по нему его в лист ListProduct, который перезаписывается в цикле           
                 ListProduct.add(ListTovar.get(i).getName());
                 ListProduct.add(ListTovar.get(i).getPrice());
+                // Лист добавляем в listGeneral для дальнейшего анализа
                 listGeneral.addAll(ListProduct);
             }
         }
         
+        // Объявляем лист для результата
         List<Double> ListResult = new ArrayList<>();
         for (int i = 0; i < setGeneral.length+1; i++) {
             if (ListGeneral[i].getPrice() > ListGeneral[i+1].getPrice()) {
+                // Если цена из одного листа внутри ListGeneral больше цены другого листа, добавляем этот лист в ListResult
                 ListResult.add(ListGeneral[i])
             }
         }
+        
+        // Выводим инфу о товаре
         for (int i = 0; i < ListResult.length; i++) {
             System.out.println(ListResult[i])
     }
